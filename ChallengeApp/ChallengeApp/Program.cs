bf7d2443509d4e7d0f729f2e7497662c7c1bb390
Gonia2312 <malgorzatabojar@gmail.com>
2023-03-11 21:10:22 +0100

    zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+var name = "Ewa";
+var age = 33;
+var gender = "kobieta";
+
+
+if (age == 33)
+{
+    if (name == "Ewa")
+      Console.WriteLine("EWA LAT 33");
+}
+
+else if (age < 30)
+{ 
+if (gender == "kobieta" && age < 30)
+
+    Console.WriteLine("KOBIETA PONIŻEJ 30 LAT");
+else if (age < 18)
+ if (gender != "kobieta"&& age < 18)
+   
+    Console.WriteLine("NIEPEŁNOLETNI MĘŻCZYZNA");
+}
+    
+
